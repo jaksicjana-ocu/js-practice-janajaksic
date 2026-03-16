b698cd90b912b4415a92e0add08e3de7946204b8
jaksicjana-ocu <jaksicjana@myocu.oak.edu>
2026-03-15 20:07:51 -0500

    js updated

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+// Favorite team (using let)
+let favoriteTeam = "Chicago Bulls";
+
+// Sport (using const)
+const sport = "Basketball";
+
+// Player object
+let player = {
+    name: "Michael Jordan",
+    position: "Shooting Guard",
+    pointsPerGame: 30.1
+};
+
+// Add new property for number of games played
+player.gamesPlayed = 1072;
+
+// Print each value to the console
+console.log("Favorite Team:", favoriteTeam);
+console.log("Sport:", sport);
+console.log("Player Name:", player.name);
+console.log("Position:", player.position);
+console.log("Points Per Game:", player.pointsPerGame);
+console.log("Games Played:", player.gamesPlayed);
